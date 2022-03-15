27256738cdf95fe9e28ca4dbc8fd8916288e5eca
samplenityagithub <101619574+samplenityagithub@users.noreply.github.com>
2022-03-15 12:07:49 +0530

    Add files via upload

---
+++ Std.java
@@ -0,0 +1,12 @@
+class Std{
+public static void main(String[] args){
+
+
+
+System.out.println("Hello students");
+System.out.println("Nitya ");
+System.out.println("Computer Science");
+System.out.println("Batch B");
+System.out.println("vishveshwarya group of institutions");
+}
+}
