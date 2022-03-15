b4a5fdbc0321e3b489914afc270a248a8e959cb2
Nitya Singh <101619574+samplenityagithub@users.noreply.github.com>
2022-03-15 12:54:34 +0530

    Add files via upload

---
+++ Mult.java
@@ -0,0 +1,10 @@
+class Mult{
+ static int mul(int n1, int n2, int n3)
+  {
+    int result=n1*n2*n3;
+    return result;}
+ public static void main(String arg[])
+ {
+   System.out.println(mul(5,8,3));
+ }
+}
